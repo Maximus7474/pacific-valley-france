c79a85813ce0bf4e5188313a95c3f8b7728f5ef8
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-23 15:31:33 +0200

    refactor(events): rename sesion handler import

--- src/events/session_handler.ts
+++ src/events/session_handler.ts
@@ -1,12 +1,12 @@
 import { Events } from "discord.js";
 import EventHandler from "../classes/event_handler";
-import session_handler from "../handlers/session_handler";
+import Sessions from "../handlers/session_handler";
 
 export default new EventHandler({
     name: 'SESSION',
     eventName: Events.InteractionCreate,
     type: "on",
     callback: (logger, client, interaction) => {
-        session_handler.HandleInteraction(client, interaction);
+        Sessions.HandleInteraction(client, interaction);
     }
 });
