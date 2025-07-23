3382bdc5557ee8456c045c49331365c94673bf05
Maximus7474 <the.maximus.7474@gmail.com>
2025-07-24 00:29:39 +0200

    feat(handlers): group handler (get, edit, add methods)

---
+++ src/handlers/group_handler.ts
@@ -0,0 +1,68 @@
+import { GenericResponse } from "@types";
+import DB from "../utils/database";
+import Logger from "../utils/logger";
+import { User } from "discord.js";
+
+const logger = new Logger('Group Handler');
+
+interface RawGroup {
+    id: number;
+    name: string;
+    acronym: string;
+    emoji: string;
+    description: string | null;
+    added_by: string;
+    added_at: number;
+};
+
+type GroupData = Omit<RawGroup, 'id' | 'added_at' | 'added_by'>;
+
+function GetGroups(): RawGroup[] {
+    return DB.all<RawGroup>('SELECT * FROM `player_groups`');
+}
+
+function EditGroup(groupId: number, data: Partial<RawGroup>): GenericResponse {
+    const currentData = DB.get<RawGroup>('SELECT * FROM `player_groups` WHERE `id` = ?', [groupId]);
+
+    if (data.acronym && data.acronym.length > 6) return { success: false, error: 'Acronym est trop long' };
+
+    const newData = {
+        ...currentData,
+        ...data,
+    };
+
+    if (newData.description && newData.description.length < 3) newData.description = null;
+
+    try {
+        const result = DB.run(
+            'UPDATE `player_groups` SET `name` = ?, `acronym` = ?, `emoji` = ?, `description` = ? WHERE `id` = ?',
+            [newData.name, newData.acronym, newData.emoji, newData.description, groupId],
+        );
+
+        if (result === 1) return { success: true };
+        else return { success: false, error: 'Impossible de mettre à jour en BDD' };
+    } catch (err) {
+        logger.error(`An error occured when updating group (${groupId}):`, (err as Error).message);
+        return { success: false, error: "Une erreure c'est produite" };
+    }
+}
+
+function AddGroup(data: GroupData, user: User): GenericResponse {
+    if (data.acronym && data.acronym.length > 6) return { success: false, error: 'Acronym est trop long' };
+    if (data.description && data.description.length < 3) data.description = null;
+
+    try {
+        const result = DB.run(
+            'INSERT INTO `player_groups` (`name`, `acronym`, `emoji`, `description`, `added_by`) VALUES (?, ?, ?, ?, ?)',
+            [data.name, data.acronym, data.emoji, data.description, user.id]
+        );
+
+        if (result !== 0) return { success: true };
+        else return { success: false, error: 'Impossible a créer en BDD' };
+    } catch (err) {
+        logger.error(`An error occured when creating a group (${data.name}):`, (err as Error).message);
+        return { success: false, error: "Une erreure c'est produite" };
+    }
+}
+
+export default { EditGroup, GetGroups, AddGroup };
