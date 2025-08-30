3c3d699588971e47f0837d3ac96ea360c2ca2f0c
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-30 19:17:32 +0200

    feat(utils/database): added whitelist-role to settings page

--- src/utils/database/sqlite-base.sql
+++ src/utils/database/sqlite-base.sql
@@ -75,4 +75,5 @@ INSERT OR IGNORE INTO `player_groups` (`name`, `acronym`, `emoji`, `added_by`)
 
 INSERT OR IGNORE INTO `settings` (`name`, `data_type`, `value`) VALUES 
     ('session_channel', 'string', '1258888271262388444'),
-    ('media-reposter-channel', 'string', '1401606221076762644');
+    ('media-reposter-channel', 'string', '1401606221076762644'),
+    ('whitelist-role', 'string', '1411397764339990699');
